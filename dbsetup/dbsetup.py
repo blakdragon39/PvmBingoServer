28458251c7c6c3c61db62167add6b4914bdb546f
Jenna Hartley <jenna.hartley@pushinteractions.com>
2018-10-12 11:54:47 -0600

    Can now populate the db

--- dbsetup/dbsetup.py
+++ dbsetup/dbsetup.py
@@ -3,17 +3,26 @@ import json
 import mysql.connector
 
 mydb = mysql.connector.connect(
-	host="localhost",
-	user="root",
-	password="",
-	database="pvmbingo"
+    host="localhost",
+    user="root",
+    password="",
+    database="pvmbingo"
 )
 cursor = mydb.cursor()
 
 subprocess.call("""mysql --user="root" --database="pvmbingo" --password="" < "setupdb.sql\"""", shell=True)
 
 with open('drops.json') as file_data:
-	json_data = json.load(file_data)
-	
-	for json_item in json_data:
-		pass
+    json_data = json.load(file_data)
+
+    for json_item in json_data:
+        boss_name, boss_file = json_item['boss'], json_item['boss_file']
+        item_name, item_file = json_item['name'], json_item['item_file']
+        drop_rate = json_item['drop_rate']
+        print "Inserting " + boss_name + " " + boss_file + " " + item_name + " " + item_file + " " + str(drop_rate)
+
+        cursor.execute("INSERT IGNORE INTO bosses (name, file) VALUES (%s, %s)", (boss_name, boss_file))
+        cursor.execute("INSERT IGNORE INTO items (name, file) VALUES (%s, %s)", (item_name, item_file))
+        cursor.execute("INSERT INTO drops (boss, item, drop_rate) VALUES (%s, %s, %s)", (boss_name, item_name, drop_rate))
+
+mydb.commit()
