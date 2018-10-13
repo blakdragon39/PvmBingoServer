8503b711b13cf21aaf94edf29a7ff75f2096fed5
Jenna Hartley <jenna.hartley@live.ca>
2018-10-13 15:00:18 -0600

    Removed db credentials from committed files

--- dbsetup/dbsetup.py
+++ dbsetup/dbsetup.py
@@ -1,10 +1,11 @@
 import json
 import mysql.connector
+from dbcreds import DB_USERNAME, DB_PASSWORD
 
 mydb = mysql.connector.connect(
     host="localhost",
-    user="root",
-    password="",
+    user=DB_USERNAME,
+    password=DB_PASSWORD,
     database="pvmbingo"
 )
 cursor = mydb.cursor()
@@ -63,7 +64,7 @@ with open('drops.json') as file_data:
         boss_name, boss_file = json_item['boss'], json_item['boss_file']
         item_name, item_file = json_item['name'], json_item['item_file']
         drop_rate = json_item['drop_rate']
-        print "Inserting " + boss_name + " " + boss_file + " " + item_name + " " + item_file + " " + str(drop_rate)
+        print("Inserting " + boss_name + " " + boss_file + " " + item_name + " " + item_file + " " + str(drop_rate))
 
         cursor.execute("""
             INSERT INTO drops (item_name, item_file, boss_name, boss_file, drop_rate) VALUES (%s, %s, %s, %s, %s)
